b9140369496488b24cd2e1cc5f5ec95145cc2487
Ben Asplund <basplund87@gmail.com>
2023-01-08 15:44:43 -0600

    pages added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import './App.css';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import {  fetchCampsites } from './features/campsites/campsiteSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
+import { fetchComments } from './features/comments/commentsSlice';
+import AboutPage from './pages/AboutPage';
+
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+    dispatch(fetchPartners());
+    dispatch(fetchPromotions());
+    dispatch(fetchComments());
+}, [dispatch])
+
+
   return (
     <div className="App">
-      Hello World
+      < Header/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/> 
+        <Route path='contact' element={<ContactPage/>} />
+        <Route path='directory' element={<CampsitesDirectoryPage/>}/>
+        <Route 
+          path='directory/:campsiteId' 
+          element={<CampsiteDetailPage />}
+        />
+        <Route path='about' element={<AboutPage/>} />
+      </Routes>
+      < Footer />
     </div>
   );
 }
